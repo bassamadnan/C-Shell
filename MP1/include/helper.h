c72d0b74c48034ebb60f8d532166e54710e0c9bf
bassam <109953791+bassamadnan@users.noreply.github.com>
2023-08-26 19:51:15 +0530

    Delete MP1 directory

--- MP1/include/helper.h
+++
@@ -1,7 +0,0 @@
-#ifndef H_H_INCLUDED
-#define H_H_INCLUDED
-#include "main.h"
-str replace(str input, char prev, str curr);
-bool cmp(str s1, str s2);
-
-#endif
--- MP1/string.c
+++
@@ -1,7 +0,0 @@
-#include "main.h"
-int main(){
-    str_arr strarr = (char**)malloc(sizeof(char*)*2);
-    strcpy(strarr[0], "Hi");
-    printf("%s", strarr[0]);
-    return 0;
-}
